bffe50b5f43b628eed38fd72cd1af01dbbfeebb7
gaoyuan <2813487482@qq.com>
2021-09-15 21:39:11 +0800

    增加了1个user类

---
+++ haha01/src/main/java/com/itheima/bean/user.java
@@ -0,0 +1,38 @@
+package com.itheima.bean;
+
+public class user {
+    private String name;
+    private int age;
+
+    public user() {
+    }
+
+    public user(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
